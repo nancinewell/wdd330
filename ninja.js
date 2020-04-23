4358920b6204c5aa86830379194f79cbf1c90328
Nanci Newell <nanci.newell@gmail.com>
2020-04-23 16:04:07 -0500

    Updated Nav, Added Quiz Ninja

---
+++ ninja.js
@@ -0,0 +1,32 @@
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+
+function start(quiz){
+    let score = 0;
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+    gameOver();
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+    function check(response,answer){
+        if(response === answer){
+        alert('Correct!');
+        score++;
+        } else {
+        alert(`Nope! The correct answer is ${answer}`);
+        }
+    }
+    function gameOver(){
+        alert(`Game Over! \nYou scored ${score} point${score !== 1 ? 's' : ''}!`);
+    }
+}
+start(quiz);
