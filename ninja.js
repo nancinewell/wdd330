9454eacdfd553f7e7df31581aa21bb0aa19bfe02
Nanci Newell <nanci.newell@gmail.com>
2020-05-03 08:30:52 -0500

    quiz ninja

--- ninja.js
+++ ninja.js
@@ -1,4 +1,47 @@
 const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonder Woman",realName: "Diana Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+];
+
+const game = {
+	start(quiz){
+		this.questions = [...quiz];
+		this.score = 0;
+		
+		//primary game loop
+		for (const question of this.questions){
+			this.question = question;
+			this.ask();
+		}
+		//end of primary game loop
+		this.gameOver();
+	},
+	ask(){
+		const question = `What is ${this.question.name}'s real name?`;
+		const response = prompt(question);
+		this.check(response);
+	}, 
+	check(response){
+		const answer = this.question.realName;
+		if(response === answer){
+			alert('Nice job!');
+			this.score++;
+		} else {
+			alert(`Sorry! A bigger fan would have known it's ${answer}.`);
+		}
+	}, 
+	gameOver(){
+		alert(`G A M E   O V E R  \nYou scored ${this.score} point${this.score !==1 ? 's' : ''}`);
+	}
+	
+}
+
+game.start(quiz);
+
+
+
+/*const quiz = [
     ["What is Superman's real name?","Clark Kent"],
     ["What is Wonder Woman's real name?","Diana Prince"],
     ["What is Batman's real name?","Bruce Wayne"]
