51f98613364403bcd727382fb28257d5c9394182
Nanci Newell <nanci.newell@gmail.com>
2020-05-08 16:42:48 -0500

    Chap 8

--- ninja.js
+++ ninja.js
@@ -6,6 +6,7 @@ const quiz = [
 
 // View Object
 const view = {
+	response: document.querySelector('#response'),
 	start: document.getElementById("start"),
 	score: document.querySelector("#score strong"),
 	question: document.getElementById("question"),
@@ -25,29 +26,61 @@ const view = {
 	},
 	hide(element){
 		element.style.display = 'none';
+	}, 
+	setup(){
+		this.show(this.question);
+		this.show(this.response);
+		this.show(this.result);
+		this.hide(this.start);
+		this.render(this.score,game.score);
+		this.render(this.result,'');
+		this.render(this.info,'');
+		this.resetForm();
+	}, 
+	resetForm(){
+		this.response.answer.value = '';
+		this.response.answer.focus();
+	},
+	teardown(){
+		this.hide(this.question);
+		this.hide(this.response);
+		this.show(this.start);
 	}
 };
 
 // Game Object
 const game = {
   start(quiz){
-    this.score = 0;
-    this.questions = [...quiz];
-    // main game loop
-    for(const question of this.questions){
+	this.score = 0;
+	this.questions = [...quiz];
+	view.setup();
+	this.ask();
+	// main game loop
+    /*for(const question of this.questions){
       this.question = question;
       this.ask();
-    }
+    }*/
     // end of main game loop
     this.gameOver();
   },
-  ask(){
+  /*ask(){
     const question = `What is ${this.question.name}'s real name?`;
     view.render(view.question,question);
     const response =  prompt(question);
     this.check(response);
-  },
-  check(response){
+  }*/
+	
+ask(name){
+    if(this.questions.length > 0) {
+        this.question = this.questions.pop();
+        const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question,question);
+    }
+    else {
+        this.gameOver();
+    }
+},
+  /*check(response){
     const answer = this.question.realName;
     if(response === answer){
       view.render(view.result,'You know your DC!',{'class':'correct'});
@@ -58,36 +91,31 @@ const game = {
       view.render(view.result,`Sorry! A bigger fan would have known the answer is ${answer}.`,{'class':'wrong'});
       alert(`Sorry! A bigger fan would have known the answer is ${answer}.`);
     }
-  },
+  }*/
+	
+	check(event){
+		event.preventDefault();
+		const response = view.response.answer.value;
+		const answer = this.question.realName;
+		if(response === answer){
+			view.render(view.result,'Correct!',{'class':'correct'});
+			this.score++;
+			view.render(view.score,this.score);
+		} else {
+			view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+		}
+		view.resetForm();
+		this.ask();
+},
   gameOver(){
     view.render(view.info,`G A M E   O V E R \nYou scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+	view.teardown();
   }
 }
 
 game.start(quiz);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
 
 
 
-
-/*const quiz = [
-    ["What is Superman's real name?","Clark Kent"],
-    ["What is Wonder Woman's real name?","Diana Prince"],
-    ["What is Batman's real name?","Bruce Wayne"]
-];
-
-let score = 0;
-
-for( const [question, answer] of quiz){
-	const response = prompt (question);
-	if(response === answer){
-		alert('Correctamundo!');
-		score++
-	} else {
-		alert(`That's a big Nope sandwich covered in Nope sauce! \nIt's really ${answer}.`);
-	}
-}
-
-//game.start(view.hide(view.start));
-//view.start.addEventListener('click', () => game.start(quiz), false);
-//view.start.addEventListener('click', () => test(), false);
-view.start.addEventListener('click', () => game.start(quiz), false);
