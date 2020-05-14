7630b8d1e5ccbab0403c468c709ca39fd7102191
Nanci Newell <nanci.newell@gmail.com>
2020-05-14 13:38:46 -0500

    update

--- graphexport.js
+++ graphexport.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-function buildGraph(edges){
+export function buildGraph(edges){
 			let graph = Object.create(null);
 			
 			function addEdge(from, to){
@@ -16,4 +16,4 @@ function buildGraph(edges){
 			return graph;
 		}
 
-export{ buildGraph };
+//export{ buildGraph };
