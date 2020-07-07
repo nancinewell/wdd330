93c4b8c9a4334ea54edd3725590e3f434ad6c093
Nanci Newell <nanci.newell@gmail.com>
2020-07-07 10:50:40 -0500

    prime & demo

--- RACKO/js/ls.js
+++ RACKO/js/ls.js
@@ -1,15 +1,18 @@
+import { scoreBoardList } from './racko_localplay_stretch.js';
+
 //Local Storage Helpers
 function pullListFromStorage(newList) {
 	//pull itemList from storage
 	let storedList = JSON.parse(localStorage.getItem("list"));
 	//set new itemList
 	if (storedList != null) {
-		newList.itemList = storedList;
+		newList = storedList;
 	}
+	return newList;
 }
 
-function setListToStorage() {
-	localStorage.setItem("list", JSON.stringify(newList.itemList));
+function setListToStorage(scoreBoardList) {
+	localStorage.setItem("list", JSON.stringify(scoreBoardList));
 }
 
 export { pullListFromStorage, setListToStorage}
--- RACKO/js/utilities.js
+++ RACKO/js/utilities.js
@@ -6,25 +6,42 @@
 	return genElement;
 }
 
-function enterKeypress(event) {
-	if (event.keyCode == 13) {
-		createNewItem();
+
+//fetch the data from the numbers API and set to the global variable "numbers"
+//retrieve with numbers[i]
+let numbers;
+fetch('http://numbersapi.com/1..60', {
+	method: 'GET',
+	//mode: 'cors',
+	redirect: 'follow',
+	cache: 'no-cache'
+})
+
+	.then(response => {
+		return response.json();
+	})
+
+	.then(numbersObject => {
+		numbers = numbersObject
+	})
+
+
+function isPrime(number) {
+	let countDivisors = 0;
+	let isPrimeNum = false;
+	for (let i = 2; i <= number; i++) {
+		if (number % i == 0) {
+			countDivisors++;
+			if (countDivisors > 1) {
+				return isPrimeNum;
+			}
+		}
 	}
+	isPrimeNum = true;
+	return isPrimeNum;
 }
 
-export { createElement, enterKeypress }
 
 
+export { createElement, numbers, isPrime }
 
-
-
-
-
-
-
-
-
-function drawTopOfDrawPile(cardContainer) {
-	newDeck.drawTopCard(cardContainer);
-	cardContainer.removeEventListener("click", drawTopOfDrawPile());
-}
